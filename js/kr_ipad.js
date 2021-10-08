76e4584c6b9bb764b56bf7214f6dba3b49db4b13
offset_t <offset_t@naver.com>
2021-10-08 09:23:39 +0900

    add

---
+++ js/kr_ipad.js
@@ -0,0 +1,28 @@
+// pad & mini models (2021.09.15)
+var MODEL_LIST = {
+'MK2K3KH/A': 'pad 64GB space gray',
+'MK2N3KH/A': 'pad 256GB space gray',
+'MK2L3KH/A': 'pad 64GB silver',
+'MK2P3KH/A': 'pad 256GB silver',
+'MK473KH/A': 'pad 64GB space gray',
+'MK4E3KH/A': 'pad 256GB space gray',
+'MK493KH/A': 'pad 64GB silver',
+'MK4H3KH/A': 'pad 256GB silver',
+
+'MK7R3KH/A': 'mini 64GB purple',
+'MLWL3KH/A': 'mini 64GB pink',
+'MK7P3KH/A': 'mini 64GB starlight',
+'MK7M3KH/A': 'mini 64GB space gray',
+'MK7X3KH/A': 'mini 256GB purple',
+'MLWR3KH/A': 'mini 256GB pink',
+'MK7V3KH/A': 'mini 256GB starlight',
+'MK7T3KH/A': 'mini 256GB space gray',
+'MK8E3KH/A': 'mini 64GB purple',
+'MLX43KH/A': 'mini 64GB pink',
+'MK8C3KH/A': 'mini 64GB starlight',
+'MK893KH/A': 'mini 64GB space gray',
+'MK8K3KH/A': 'mini 256GB purple',
+'MLX93KH/A': 'mini 256GB pink',
+'MK8H3KH/A': 'mini 256GB starlight',
+'MK8F3KH/A': 'mini 256GB space gray'
+};
